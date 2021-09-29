108f34e2be820db9462a9dfedf2421e4edfc7edf
Smirnova Nina <ms.nina.smirnova@gmail.com>
2021-09-29 14:31:49 +0200

    add style and js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-require('../../resources/js/bootstrap');
+window.onscroll = function() {myFunction()};
+        
+var header = document.getElementById("fixedHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
